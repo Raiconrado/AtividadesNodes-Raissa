4f7565e10be046870aa6bc0b3431642b0349db68
Raiconrado <raissa.conrado06@gmail.com>
2024-10-08 23:06:49 +0000

    08\10\2024

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ console.log("Olá meu nome é " + NomeCompleto + ", tenho " + Idade + " anos " +
 
 console.log(`oi eu sou ${nomeCompleto} meu ano de nascimento é ${anoNascimento} minha idade é ${idade} `);
 
+const loginCerto = "Raissa";
+const senhaCerta = 12345;
+console.log("                               -------------------");
+console.log("                               AGENCIA DE VIAGENS")
+console.log("                               ------------------- ");
+console.log("Faça seu login");
+
+var login = entrada("Digite seu login: ");
+var senha = entrada("Digite sua senha: ");
+
+while(login != loginCerto){
+  console.log("Login Incorreto");
+  login = entrada("Digite seu login novamente: ");
+}
+
+while(senha != senhaCerta){
+  console.log("senha incorreta: ");
+}
+
 const listaDeViagens = new Array (
   `Estados Unidos`,
   `Dubai`, 
@@ -44,5 +63,6 @@ if(idadeComprador < 18){
   console.log(`Olá ${nomeComprador} vocẽ é de menor,não podera comprar as passagens já que a sua idade é ${idadeComprador} anos`);
 }
 if(idadeComprador >= 18){
-  console.log(`Olá senhor ${nomeComprador} podemos continuar com a sua compra, qual o seu destino?`)
+  console.log(`Olá senhor(a) ${nomeComprador} podemos continuar com a sua compra, qual o seu destino?`)
 }
+
